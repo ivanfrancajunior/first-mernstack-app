aff41a3fad5e1378bc64897464868cb7103cc1ae
Ivan França <104383865+ivanfrancajunior@users.noreply.github.com>
2023-02-09 20:48:07 -0300

    feat:  added a schema for the data in the db

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,22 @@
+const userService = require('../services/user.services')
 
-const create = (req, res) => {
+
+const create = async (req, res) => {
   const {name,username,email,password,avatar,background} = req.body;
   
  if(!name || !username || !email || !avatar || !background || !password){
     res.status(400).send({"message":"Submit all fields for registration"})
  }
  
+  const user = await userService.create(req.body)
+
+  if(!user){
+    return res.status(400).send({message:"Error creating User"})
+  }
 
   res.status(201).send({
     message: "User created!",
-    user: { name, username, email, avatar, background },
+    user: {id:user._id,name, username, email, avatar, background},
   });
 }
 
