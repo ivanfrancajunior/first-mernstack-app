ce758deaa0322cc5d75b56af2c02b24c78d8c31f
Ivan França <104383865+ivanfrancajunior@users.noreply.github.com>
2023-02-10 16:59:50 -0300

    feat: add try/catch to functions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const userService = require('../services/user.services')
 
 const create = async (req, res) => {
-  const {name,username,email,password,avatar,background} = req.body;
+  try {const {name,username,email,password,avatar,background} = req.body;
   
  if(!name || !username || !email || !avatar || !background || !password){
     res.status(400).send({"message":"Submit all fields for registration"})
@@ -17,25 +17,33 @@ const create = async (req, res) => {
     message: "User created!",
     user: {id:user._id,name, username, email, avatar, background},
   });
+  } catch(error){
+    res.status(500).send({message: err.message})
+  }
 }
 
 const findAll = async(req,res) => {
- const users = await userService.findAllService();
+ try {const users = await userService.findAllService();
 
  if(users.length === 0){
   return res.status(400).send({message:"There are not registred users"})
  }
- res.send(users)
+ res.send(users)} catch(error){
+  res.status(500).send({message: err.message})
+} 
 }
 
 const findById = async(req,res) =>{
-  const {id, user} = req 
+  try {const {id, user} = req 
 
   res.send(user)
+}catch(error){
+  res.status(500).send({message: err.message})
+} 
 }
 
 const update = async (req,res) =>{
-  const {name,username,email,password,avatar,background} = req.body;
+  try {const {name,username,email,password,avatar,background} = req.body;
   const {id, user} = req 
   
   if(!name && !username && !email && !avatar && !background && !password){
@@ -51,7 +59,10 @@ const update = async (req,res) =>{
     password,
     avatar,
     background
-  );
+  );} 
+  catch(error){
+    res.status(500).send({message: err.message})
+  } 
 
   res.send({message: "User succesfully updated!"})
 
--- src/middlewares/global.middleware.js
+++ src/middlewares/global.middleware.js
@@ -1,31 +1,35 @@
-const mongoose = require ('mongoose')
-const userService = require('../services/user.services')
+const mongoose = require("mongoose");
+const userService = require("../services/user.services");
 
-const validId = (req,res,next) =>{
-    const id = req.params.id; 
+const validId = (req, res, next) => {
+  try {
+    const id = req.params.id;
 
-  if(!mongoose.Types.ObjectId.isValid(id)){
-    return res.status(400).send({message:"Invalid ID"})
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send({ message: "Invalid ID" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: err.message });
   }
 
- next()
-}
+  next();
+};
 
-
-const validUser = async (req,res,next) =>{
-    const id = req.params.id; 
+const validUser = async (req, res, next) => {
+  try {
+    const id = req.params.id;
 
     const user = await userService.findByIdService(id);
 
-    if(!user) {
-        return res.status(400).send({message:"User not Found"})
+    if (!user) {
+      return res.status(400).send({ message: "User not Found" });
     }
+  } catch (error) {
+    res.status(500).send({ message: err.message });
+  }
+  req.id = id;
+  req.user = user;
+  next();
+};
 
-
-    req.id = id;
-    req.user = user
-    next()  
-
-}
-
-module.exports = {validId,validUser}
+module.exports = { validId, validUser };
