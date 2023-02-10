6fa1992a7d46c277b2de2869b756a0937c5853f5
Ivan França <104383865+ivanfrancajunior@users.noreply.github.com>
2023-02-10 16:20:30 -0300

    feat: add user control middlewares

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,4 @@
 const userService = require('../services/user.services')
-const mongoose = require('mongoose')
 
 const create = async (req, res) => {
   const {name,username,email,password,avatar,background} = req.body;
@@ -32,15 +31,7 @@ const findAll = async(req,res) => {
 const findById = async(req,res) =>{
   const id = req.params.id; 
 
-  if(!mongoose.Types.ObjectId.isValid(id)){
-    return res.status(400).send({message:"Invalid ID"})
-  }
-  
   const user = await userService.findByIdService(id);
-
-  if(!user) {
-    return res.status(400).send({message:"User not Found"})
-  }
 
   res.send(user)
 }
@@ -53,14 +44,8 @@ const update = async (req,res) =>{
   }
   const id = req.params.id; 
 
-  if(!mongoose.Types.ObjectId.isValid(id)){
-    return res.status(400).send({message:"Invalid ID"})
-  }
   const user = await userService.findByIdService(id);
 
-  if(!user) {
-    return res.status(400).send({message:"User not Found"})
-  }
 
   await userService.updateService(
     id,
---
+++ src/middlewares/global.middleware.js
@@ -0,0 +1,28 @@
+const mongoose = require ('mongoose')
+const userService = require('../services/user.services')
+
+const validId = (req,res,next) =>{
+    const id = req.params.id; 
+
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(400).send({message:"Invalid ID"})
+  }
+
+ next()
+}
+
+
+const validUser = async (req,res,next) =>{
+    const id = req.params.id; 
+
+    const user = await userService.findByIdService(id);
+
+    if(!user) {
+        return res.status(400).send({message:"User not Found"})
+      }
+
+    next()  
+
+}
+
+module.exports = {validId,validUser}
