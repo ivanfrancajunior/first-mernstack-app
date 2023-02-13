4cd99699165ef0f88cba27068658736b9886f478
Ivan França <104383865+ivanfrancajunior@users.noreply.github.com>
2023-02-13 15:45:33 -0300

    feat: add JWT and logic to generate the token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt')
-const {loginService} = require('../services/auth.service')
+const {loginService, genereteToken} = require('../services/auth.service')
 
 
 const login = async (req,res) => {
@@ -7,17 +7,19 @@ const login = async (req,res) => {
     
     try {
       const user = await loginService(email);
-
+      
       const passwordIsValid =  bcrypt.compareSync(password, user.password);
-
+      
       if(!passwordIsValid){
-        return res.status(404).send({message:'user or password invalid'})
-      }
-      if(!user){
-        return res.status(404).send({message:'user or password invalid'})
-      }
+          return res.status(404).send({message:'user or password invalid'})
+        }
+        if(!user){
+            return res.status(404).send({message:'user or password invalid'})
+        }
+        
+        const token = genereteToken(user.id)
+        res.send({token});
 
-    res.send('Login ok!');
     } catch (err) {
       res.status(500).send(err.message);
     }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,9 @@
-const User = require('../models/User')
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
 
 const loginService = (email) => User.findOne({email:email}).select('+password')
 
-module.exports = {loginService}
+const genereteToken = (id) => jwt.sign({id:id},process.env.MD5HASHKEY,{expiresIn: 86400})
+
+module.exports = {loginService, genereteToken}
