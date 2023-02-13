08b0f3a5fc236663036da82220ca727bb2c75d4d
Ivan França <104383865+ivanfrancajunior@users.noreply.github.com>
2023-02-13 14:31:34 -0300

    feat adds validation  login and password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -10,9 +10,14 @@ const login = async (req,res) => {
 
       const passwordIsValid =  bcrypt.compareSync(password, user.password);
 
-      console.log(passwordIsValid);
+      if(!passwordIsValid){
+        return res.status(404).send({message:'user or password invalid'})
+      }
+      if(!user){
+        return res.status(404).send({message:'user or password invalid'})
+      }
 
-      res.send({ user });
+    res.send('Login ok!');
     } catch (err) {
       res.status(500).send(err.message);
     }
